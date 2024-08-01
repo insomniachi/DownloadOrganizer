e8d4d1c1b523ef4d54f5a38711a8a4a6c2f06fcc
athulrajts <athulrajts@gmail.com>
2024-08-01 17:03:28 +0530

    more ignored files

--- src/DownloadOrganizer/FileExtensions.cs
+++ src/DownloadOrganizer/FileExtensions.cs
@@ -2,7 +2,7 @@
 
 internal static class FileExtensions
 {
-	public static string[] IgnoredFiles { get; } = [".txt"];
+	public static string[] IgnoredFiles { get; } = [".txt", ".nfo", ".jpg"];
 	public static string[] MediaFiles { get; } = [".mkv", ".mp4"];
 	public static string Zip { get; } = ".zip";
 }
