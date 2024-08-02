3d95ae38d8fc5bc78119ef9cbc4df5dea1d540a8
athulrajts <athulrajts@gmail.com>
2024-08-02 20:28:42 +0530

    use console app framework

---
+++ src/DownloadOrganizer/Commands.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleAppFramework;
+
+namespace DownloadOrganizer;
+
+public static class Commands
+{
+	/// <summary>
+	/// Move a single directory/archive to media folder
+	/// </summary>
+	/// <param name="directoryToMove">full path to directory/archive</param>
+	public static void MoveDirectory([Argument] string directoryToMove)
+	{
+		if (string.IsNullOrEmpty(directoryToMove))
+		{
+			return;
+		}
+
+		Organizer.Organize(directoryToMove);
+	}
+
+	/// <summary>
+	/// Organize the contents of your downloads directory into the media directory in a single go
+	/// </summary>
+	/// <param name="downloadsDirectory">full path to downloads directory</param>
+	/// <param name="mediaDirectory">base path of media directory, subdirectories will be created by the tool</param>
+	public static void OrganizeDownloadsDirectory([Argument] string downloadsDirectory, [Argument] string mediaDirectory)
+	{
+		if (string.IsNullOrEmpty(downloadsDirectory) || string.IsNullOrEmpty(mediaDirectory))
+		{
+			return;
+		}
+
+		Organizer.Organize(downloadsDirectory, mediaDirectory);
+	}
+
+	/// <summary>
+	///  Organize the contents of your downloads directory into the media directory in a single go configured in appsettings
+	/// </summary>
+	public static void Run() => Organizer.Organize();
+}
--- src/DownloadOrganizer/Program.cs
+++ src/DownloadOrganizer/Program.cs
@@ -1,38 +1,17 @@
-﻿using DownloadOrganizer;
+﻿using ConsoleAppFramework;
+using DownloadOrganizer;
 
+var app = ConsoleApp.Create();
+
+app.Add("mvd", Commands.MoveDirectory);
+app.Add("", Commands.OrganizeDownloadsDirectory);
+app.Add("run", Commands.Run);
 
 try
 {
-	if (args is { Length: 0 })
-	{
-		Organizer.Organize();
-	}
-	else if (args is { Length: 1 })
-	{
-		var completedDownload = args[0];
-		if(string.IsNullOrEmpty(completedDownload))
-		{
-			return;
-		}
-
-		Organizer.Organize(completedDownload);
-	}
-	else if (args is { Length: 2 })
-	{
-		var sourceDir = args[0];
-		var targetDir = args[1];
-		if (string.IsNullOrEmpty(sourceDir) || string.IsNullOrEmpty(targetDir))
-		{
-			return;
-		}
-
-		Organizer.Organize(sourceDir, targetDir);
-	}
+	app.Run(args);
 }
 catch (Exception ex)
 {
 	File.WriteAllText("DownloadOrganizerError.txt", ex.Message + Environment.NewLine + ex.StackTrace);
 }
-
-
-
