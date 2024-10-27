8cc8d51c36cdf70a1c5164c54a9a791f7c76f7d5
athulrajts <athulrajts@gmail.com>
2024-10-27 11:39:53 +0530

    cleanup + ad ip command

--- src/Aria2TelegramBot/UpdateHandler.cs
+++ src/Aria2TelegramBot/UpdateHandler.cs
@@ -1,24 +1,28 @@
 ﻿using Aria2NET;
 using System.Diagnostics;
 using System.Net.Sockets;
+using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Aria2TelegramBot;
 
-public class UpdateHandler(Aria2NetClient aria2) : IUpdateHandler
+public class UpdateHandler(Aria2NetClient aria2, 
+	                       IHttpClientFactory httpClientFactory,
+	                       IConfiguration configuration) : IUpdateHandler
 {
-	// private readonly long[] _admins = [147093641];
-	// private readonly string[] _declineMessages =
-	// [
-	// 	"I would love to say yes, but my dog told me to say no.",
-	// 	"I would, but I’m a teapot!",
-	// 	"418",
-	// 	"Request rejected!",
-	// 	"My parents would disown me if I did that.",
-	//
-	// ];
+	private readonly long _admin = configuration.GetValue<long>("TelegramAdmin");
+	
+	private readonly string[] _declineMessages =
+	[
+		"I would love to say yes, but my dog told me to say no.",
+		"I would, but I’m a teapot!",
+		"418",
+		"Request rejected!",
+		"My parents would disown me if I did that.",
+	];
 
 	public Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
 	{
@@ -35,10 +39,27 @@ public class UpdateHandler(Aria2NetClient aria2) : IUpdateHandler
 		});
 	}
 
-	// private bool IsAuthorized(long id)
-	// {
-	// 	return _admins.Contains(id);
-	// }
+	private async Task<bool> IsAuthorized(ITelegramBotClient bot, Message message)
+	{
+		var chatId = message.Chat.Id;
+		
+		if (message.From is not { } user)
+		{
+			return false;
+		}
+		
+		var userId = user.Id;
+
+		if (userId == _admin)
+		{
+			return true;
+		}
+
+		var administrators = await bot.GetChatAdministratorsAsync(chatId);
+
+		return administrators.Any(x => x.User.Id == userId) &&
+		       administrators.FirstOrDefault(x => x.User.Id == _admin) is not null;
+	}
 
 	private async Task OnMessage(ITelegramBotClient bot, Message message)
 	{
@@ -46,58 +67,91 @@ public class UpdateHandler(Aria2NetClient aria2) : IUpdateHandler
 		{
 			return;
 		}
-
+		
 		var chatId = message.Chat.Id;
 		var parts = messageText.Split(' ');
 		
-		if (messageText is "/aria2")
+		switch (messageText)
 		{
-			await GetAria2Version(bot, chatId, message);
-		}
-		else if (messageText.StartsWith("/download"))
-		{
-			if (parts.Length < 2)
+			case "/aria2":
+				await ReplyAria2Version(bot, chatId, message);
+				break;
+			case "/status":
+				await ServiceStatus(bot, chatId, message);
+				break;
+			case "/wol":
+				await WakeOnLan(bot, chatId, message);
+				break;
+			case "/ip":
+				await ReplyIp(bot, chatId, message);
+				break;
+			default:
 			{
-				await bot.SendTextMessageAsync(chatId, "/download <url>", replyParameters: message);
+				if (messageText.StartsWith("/download"))
+				{
+					await DownloadUrl(bot, chatId, message, parts);
+				}
+				else if(messageText.StartsWith("/dlstat"))
+				{
+					await ReplyDownloadStatus(bot, chatId, message, parts);
+				}
+				break;
 			}
-
-			await Download(bot, chatId, message, parts[2]);
-		}
-		else if(messageText.StartsWith("/dlstat"))
-		{
-			if (parts.Length < 2)
-			{
-				await bot.SendTextMessageAsync(chatId, "/dlstat <gid>", replyParameters: message);
-			}
-
-			await DownloadStatus(bot, chatId, message, parts[2]);
-		}
-		else if(messageText is "/status")
-		{
-			await ServiceStatus(bot, chatId, message);
-		}
-		else if(messageText is "/wol")
-		{
-			await WakeOnLan(bot, chatId, message);
 		}
 	}
 
-	private async Task Download(ITelegramBotClient bot, long chatId, Message messageId, string url)
+	private async Task DownloadUrl(ITelegramBotClient bot, long chatId, Message message, string[] parts)
 	{
+		if (!await IsAuthorized(bot, message))
+		{
+			await RequestDenied(bot, message);
+			return;
+		}
+					
+		if (parts.Length < 2)
+		{
+			const string reply = "/download ❌\n/download <url> ✅";
+			await bot.SendTextMessageAsync(chatId, reply, replyParameters: message);
+			return;
+		}
+
+		var url = parts[2];
 		var id = await aria2.AddUriAsync([url]);
-		await bot.SendTextMessageAsync(chatId, $"download queued gid : `{id}`", replyParameters: messageId, parseMode: Telegram.Bot.Types.Enums.ParseMode.Markdown);
+		await bot.SendTextMessageAsync(chatId, $"download queued gid : `{id}`", replyParameters: message, parseMode: ParseMode.Markdown);
 	}
 
-	private async Task GetAria2Version(ITelegramBotClient bot, long chatId, Message messageId)
+	private async Task ReplyAria2Version(ITelegramBotClient bot, long chatId, Message message)
 	{
 		var version = await aria2.GetVersionAsync();
-		await bot.SendTextMessageAsync(chatId, version.Version, replyParameters: messageId);
+		await bot.SendTextMessageAsync(chatId, version.Version, replyParameters: message);
 	}
 
-	private async Task DownloadStatus(ITelegramBotClient bot, long chatId, Message messageId, string gid)
+	private async Task ReplyDownloadStatus(ITelegramBotClient bot, long chatId, Message message, string[] parts)
 	{
+		if (parts.Length < 2)
+		{
+			const string reply = "/dlstat ❌\n/dlstat <gid> ✅";
+			await bot.SendTextMessageAsync(chatId, reply, replyParameters: message);
+			return;
+		}
+		
+		var gid = parts[2];
 		var status = await aria2.TellStatusAsync(gid);
-		await bot.SendTextMessageAsync(chatId, $"{status.Gid} : {(status.CompletedLength/status.TotalLength) * 100 :F2}", replyParameters: messageId);
+		await bot.SendTextMessageAsync(chatId, $"{status.Gid} : {(status.CompletedLength/status.TotalLength) * 100 :F2}", replyParameters: message);
+	}
+
+	private async Task ReplyIp(ITelegramBotClient bot, long chatId, Message message)
+	{
+		var client = httpClientFactory.CreateClient();
+		var ip = await client.GetStringAsync("https://icanhazip.com/");
+		await bot.SendTextMessageAsync(chatId, ip, replyParameters: message);
+	}
+
+	private async Task RequestDenied(ITelegramBotClient bot, Message message)
+	{
+		var randomIndex = Random.Shared.Next(0, _declineMessages.Length - 1);
+		var declineMessage = _declineMessages[randomIndex];
+		await bot.SendTextMessageAsync(message.Chat.Id, declineMessage, replyParameters: message);
 	}
 
 	private static async Task ServiceStatus(ITelegramBotClient bot, long chatId, Message message)
@@ -109,7 +163,7 @@ public class UpdateHandler(Aria2NetClient aria2) : IUpdateHandler
 				{GetRow("Jellyfin", Ping(ip, 8096))}
 				{GetRow("Aria2", Ping(ip, 6800))}
 				</pre>
-				""", parseMode: Telegram.Bot.Types.Enums.ParseMode.Html, replyParameters: message);
+				""", parseMode: ParseMode.Html, replyParameters: message);
 		return;
 
 		static string GetRow(string key, string value)
